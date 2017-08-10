f4a6ff66bdd77846292eb1c9b863dcfcf18a5185
dennysw <dennysetiawisnugraha@gmail.com>
2017-08-10 13:22:28 +0700

    kasubag seleksi

--- application/controllers/kasubag/C_seleksi.php
+++ application/controllers/kasubag/C_seleksi.php
@@ -5,7 +5,7 @@ class C_seleksi extends CI_Controller
   function __construct()
   {
     parent::__construct();
-    $this->load->model("kasubag/Requested",'mdl');
+    $this->load->model("kasubag/Seleksi",'mdl');
   }
 
   public function index()
@@ -13,6 +13,55 @@ class C_seleksi extends CI_Controller
     $this->load->view('attribute/header_kasubag');
     $this->load->view('kasubag/seleksi');
     $this->load->view('attribute/footer');
+  }
+
+  public function datatable(){
+    $fetch_data = $this->mdl->make_datatables();
+    $data = array();
+    $nmr = 0;
+    foreach($fetch_data as $row)
+    {
+      $nmr+=1;
+      $sub_array = array();
+      $sub_array[] = $nmr;
+      $sub_array[] = $row->namaBeasiswa;
+      $sub_array[] = $row->penyelenggaraBea;
+      if ($row->selektor == "1") {
+        # kasubag kemahasiswaan
+        $sub_array[] = '<i class="mdi-toggle-radio-button-on" title="Kasubag. Kemahasiswaan"></i> Kms';
+      }elseif ($row->selektor == "2") {
+        # kasubag kemahasiswaan fakultas
+        $sub_array[] = '<i class="mdi-toggle-radio-button-off" title="Kasubag. Kemahasiswaan Fakultas"></i> Fks';
+      }elseif ($row->selektor == "3") {
+        # keduanya
+        $sub_array[] = '<i class="mdi-action-stars" title="Keduanya"></i> 2K';
+      }
+
+      $sub_array[] = '<span class="blue-text">'.$row->kuota.'</span>';
+      $status = $row->statusBeasiswa;
+      if ($status=="0" || $status=="1" ||$status=="3") {
+        # aktif/confirmed
+        $sub_array[] = '
+          <a class="btn-floating waves-effect waves-light primary-color z-depth-0" title="Confirmed"><i class="mdi-action-done"></i></a>
+          ';
+      }else{
+        # ditolak
+        $alamat = base_url('kasubag/C_requested/pengaturan');
+        $sub_array[] = '
+            <form action="'.$alamat.'" method="post">
+              <button class="btn-floating waves-effect waves-light red" title="Not Confirmed" type="submit" name="idPengaturan" value="'.$row->id.'"><i class="mdi-action-settings"></i></button>
+            </form>
+          ';
+      }
+      $data[] = $sub_array;
+    }
+    $output = array(
+      "draw"            =>  intval($_POST["draw"]),
+      "recordsTotal"    =>  $this->mdl->get_all_data(),
+      "recordsFiltered" =>  $this->mdl->get_filtered_data(),
+      "data"            =>  $data
+    );
+    echo json_encode($output);
   }
 
 }
--- application/views/kasubag/seleksi.php
+++ application/views/kasubag/seleksi.php
@@ -4,48 +4,18 @@
     <div id="dashboard">
       <div class="section">
         <div id="responsive" class="section">
-          <h4>
-            Seleksi Penerima Beasiswa
-          </h4>
           <div class="row">
-            <div class="col m12">
-              <div class="card-panel"> <!-- warna card -->
-                <div class="row">
-                  <div class="input-field col m3">
-                    <select>
-                      <option value="" disabled selected>Choose your option</option>
-                      <option value="1">Option 1</option>
-                      <option value="2">Option 2</option>
-                      <option value="3">Option 3</option>
-                    </select>
-                    <label>Nama Beasiswa</label>
-                  </div>
-                  <div class="input-field col m3">
-                    <select>
-                      <option value="" disabled selected>Choose your option</option>
-                      <option value="1">Option 1</option>
-                      <option value="2">Option 2</option>
-                      <option value="3">Option 3</option>
-                    </select>
-                    <label>Tahun</label>
-                  </div>
-                  <div class="input-field col m3">
-                    <select>
-                      <option value="" disabled selected>Choose your option</option>
-                      <option value="1">Option 1</option>
-                      <option value="2">Option 2</option>
-                      <option value="3">Option 3</option>
-                    </select>
-                    <label>Materialize Select</label>
-                  </div>
-                  <div class="input-field col m1">
-                    <button class="btn-floating waves-effect waves-light orange" onclick="add_data()" title="Tambah Data"><i class="material-icons">search</i></button>
-                  </div>
-                  <div class="input-field col m2">
-                    <small class="blue-text">* Atur filter dahulu untuk menampilkan data!</small>
-                  </div>
-                </div>
-              </div>
+            <div class="col s6">
+              <h4>
+                Seleksi Penerima Beasiswa
+              </h4>
+            </div>
+            <div class="col s6">
+              <p>
+                <blockquote>
+                <font size="4pt">Keterangan: </font><br> <i class="mdi-toggle-radio-button-on"></i> = Kasubag. Kemahasiswaan. <br> <i class="mdi-toggle-radio-button-off"></i> = Kasubag. Kemahasiswaan Fakultas. <br> <i class="mdi-action-stars"></i> = Keduanya.
+                </blockquote>
+              </p>
             </div>
           </div>
         <div class="row">
@@ -57,47 +27,11 @@
                     <th data-field="bea">Beasiswa</th>
                     <th data-field="penyelenggara">Penyelenggara</th>
                     <th data-field="selektor">Selektor</th>
-                    <th data-field="konfirmasi">Keterangan</th>
+                    <th data-field="konfirmasi">Kuota</th>
                     <th data-field="status">Status</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>BRI</td>
-                  <td>Bank BRI</td>
-                  <td><i class="material-icons" title="Kasubag. Kemahasiswaan">radio_button_checked</i></td>
-                  <td class="success-text">Telah Dikonfirmasi</td>
-                  <td>
-                    <a class="btn-floating waves-effect waves-light primary-color z-depth-0" title="Confirmed"><i class="material-icons">done</i></a>
-                  </td
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Prestasi</td>
-                  <td>UIN Maliki</td>
-                  <td><i class="material-icons" title="Kasubag. Kemahasiswaan Fakultas">radio_button_unchecked</i></td>
-                  <td class="alert-text" style="max-width: 330px;">Scoring pekerjaan orang tua belum masuk</td>
-                  <td>
-                    <form action="<?php echo base_url('kasubag/C_requested/pengaturan')?>" method="post">
-                      <button class="btn-floating waves-effect waves-light red" title="Not Confirmed" type="submit" name="idPengaturan" value="bismillah"><i class="material-icons">settings</i></button>
-                    </form>
-                  </td
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>BNI</td>
-                  <td>Bank BNI</td>
-                  <td>
-                    <i class="material-icons" title="Keduanya">star</i>
-                  </td>
-                  <td class="alert-text" style="max-width: 330px;"></td>
-                  <td>
-                    <form action="<?php echo base_url('kasubag/C_requested/pengaturan')?>" method="post">
-                      <button class="btn-floating waves-effect waves-light red" title="Not Confirmed" type="submit" name="idPengaturan" value="bisa"><i class="material-icons">settings</i></button>
-                    </form>
-                  </td
-                </tr>
               </tbody>
             </table>
           </div>
@@ -114,7 +48,7 @@
 var dataTable;
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  // datatable();
+  datatable();
 });
 
 function datatable() {
@@ -123,7 +57,7 @@ function datatable() {
     "serverSide":true,
     "order":[],
     "ajax":{
-        url:"<?php echo base_url('kasubag/C_requested/datatable'); ?>",
+        url:"<?php echo base_url('kasubag/C_seleksi/datatable'); ?>",
         type:"POST"
     },
     "columnDefs":[
