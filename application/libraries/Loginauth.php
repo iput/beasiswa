de00a4eb463cd28fb934848d0538dfd8c4e84026
dennysw <dennysetiawisnugraha@gmail.com>
2017-10-02 09:51:17 +0700

    perbaikan Loginauth

--- application/libraries/Loginauth.php
+++ application/libraries/Loginauth.php
@@ -27,8 +27,23 @@ class Loginauth{
     }elseif ($this->ci->session->userdata('level')=='6') {
       redirect('C_admin');
     }else{
-      redirect('Login');
+      $url =  $_SERVER["REQUEST_URI"];
+      if ($url!='/beasiswa/Login') {
+        redirect('Login');
+      }
     }
+  }
+
+  function curPageURL() {
+    $pageURL = 'http';
+    if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+    $pageURL .= "://";
+    if ($_SERVER["SERVER_PORT"] != "80") {
+      $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+    } else {
+      $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+    }
+    return $pageURL;
   }
 
 }
